ceabc209464688b12bfbe57106627d2b375603c3
Brian Ray Xu <33497224+brianrayxu@users.noreply.github.com>
2021-04-12 18:32:00 -0400

    Update serverP2P.py

--- serverP2P.py
+++ serverP2P.py
@@ -5,12 +5,27 @@
 
 import socket
 from threading import *
+import pymongo
+import datetime
 
-HOST = '127.0.0.1'
-PORT = 12348
+client = pymongo.MongoClient('mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb')
+db = client['test']
 
+#discovery
+discoveryclient = pymongo.MongoClient('mongodb+srv://Brian:cyp1b1@cluster0.v056q.mongodb.net/FileDB?retryWrites=true&w=majority')
+discoverydb = discoveryclient['FileDB']
+collection = discoverydb['Users']
+
+collection.find
+
+
+# Luke running server, remote client
+HOST = '155.41.17.114'
+PORT = 5000
+print(db)
 #Client socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #reset tcp server
 s.bind((HOST,PORT))
 
 class client(Thread):
@@ -21,10 +36,20 @@ class client(Thread):
 		self.start()
 
 	def run(self):
+		print("Running")
 		while True:
-			print('User 1 sent: ', self.s.recv(1024).decode())
+			data = self.s.recv(1024).decode()
+			print('User 1 sent: ', data)
 			msg = input('Enter a message: ')
 			self.s.send(msg.encode()) 
+			db.messages.insert_one(
+    			{
+					"Sender" : "Alice",
+					"Receiver" : "Bob",
+					"Text" : data,
+					"Timestamp" : datetime.datetime.now()
+				}
+)
 
 s.listen(5)
 
